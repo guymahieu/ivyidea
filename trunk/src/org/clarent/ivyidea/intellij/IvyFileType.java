58fab05b09aa2e2f78d16c01f577d97373342bd2
guy.mahieu <guy.mahieu@66ac992a-87ea-11dd-a302-d386e5130c1c>
2008-09-22 19:12:06 +0000

    Facet: implemented configuration saving/loading + editor tab (detection does not seem to work yet...)

--- trunk/src/org/clarent/ivyidea/intellij/IvyFileType.java
+++ trunk/src/org/clarent/ivyidea/intellij/IvyFileType.java
@@ -2,6 +2,7 @@ package org.clarent.ivyidea.intellij;
 
 import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.fileTypes.UserFileType;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -12,6 +13,13 @@ import org.jetbrains.annotations.Nullable;
  */
 
 public class IvyFileType extends UserFileType {
+
+    public static final FileType IVY_FILE_TYPE = new IvyFileType();
+
+    private IvyFileType() {
+        setName("Ivy");
+        setDescription("Ivy File");
+    }
 
     public SettingsEditor getEditor() {
         return null;

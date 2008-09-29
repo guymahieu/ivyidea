995568033d8a6c21de1a5d80dc02b4f044069aa2
guy.mahieu <guy.mahieu@66ac992a-87ea-11dd-a302-d386e5130c1c>
2008-09-29 16:23:56 +0000

    Added toolwindow for IvyIDEA output; nothing in it for now.

---
+++ src/org/clarent/ivyidea/intellij/ui/IvyIdeaToolWindow.java
@@ -0,0 +1,15 @@
+package org.clarent.ivyidea.intellij.ui;
+
+import javax.swing.*;
+
+/**
+ * @author Guy Mahieu
+ */
+
+public class IvyIdeaToolWindow {
+    private JPanel root;
+
+    public JPanel getRoot() {
+        return root;
+    }
+}

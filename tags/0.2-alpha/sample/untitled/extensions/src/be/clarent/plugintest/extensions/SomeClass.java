d8c1e881e2bd527ae28a10168441521570475f4f
guy.mahieu <guy.mahieu@66ac992a-87ea-11dd-a302-d386e5130c1c>
2008-10-20 19:23:10 +0000

    Release 0.2-alpha

---
+++ tags/0.2-alpha/sample/untitled/extensions/src/be/clarent/plugintest/extensions/SomeClass.java
@@ -0,0 +1,8 @@
+package be.clarent.plugintest.extensions;
+
+import be.clarent.plugintest.Test;
+
+public class SomeClass {
+
+    private Test test;
+}
---
+++ tags/0.2-alpha/src/org/clarent/ivyidea/intellij/ToolWindowRegistrationComponent.java
@@ -0,0 +1,52 @@
+package org.clarent.ivyidea.intellij;
+
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.peer.PeerFactory;
+import com.intellij.ui.content.Content;
+import org.clarent.ivyidea.intellij.ui.IvyIdeaIcons;
+import org.clarent.ivyidea.intellij.ui.IvyIdeaToolWindow;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Guy Mahieu
+ */
+
+public class ToolWindowRegistrationComponent implements ProjectComponent {
+    private Project project;
+
+    public ToolWindowRegistrationComponent(Project project) {
+        this.project = project;
+    }
+
+    public void initComponent() {
+    }
+
+    public void disposeComponent() {
+    }
+
+    @NotNull
+    public String getComponentName() {
+        return "IvyIDEA.ToolWindowRegistrationComponent";
+    }
+
+    public void projectOpened() {
+        registerToolWindow();
+    }
+
+    public void projectClosed() {
+    }
+
+    private void registerToolWindow() {
+        ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
+        ToolWindow toolWindow = toolWindowManager.registerToolWindow("IvyIDEA", false, ToolWindowAnchor.BOTTOM);
+        PeerFactory peerFactory = PeerFactory.getInstance();
+        Content content = peerFactory.getContentFactory().createContent(new IvyIdeaToolWindow().getRoot(), "IvyIDEA Console", true);
+        toolWindow.setIcon(IvyIdeaIcons.MAIN_ICON_SMALL);
+        toolWindow.getContentManager().addContent(content);
+    }
+
+}

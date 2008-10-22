de2ee79e8ccda0af17422f9014523a835e6dae4e
guy.mahieu <guy.mahieu@66ac992a-87ea-11dd-a302-d386e5130c1c>
2008-10-22 15:48:15 +0000

    0.4-alpha release

---
+++ tags/0.4-alpha/src/org/clarent/ivyidea/RemoveAllIvyIdeaModuleLibrariesAction.java
@@ -0,0 +1,42 @@
+package org.clarent.ivyidea;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.roots.libraries.LibraryTable;
+import org.clarent.ivyidea.config.IvyIdeaTempConfiguration;
+import org.clarent.ivyidea.intellij.IntellijUtils;
+
+/**
+ * @author Guy Mahieu
+ */
+
+public class RemoveAllIvyIdeaModuleLibrariesAction extends AnAction {
+
+    public void actionPerformed(AnActionEvent e) {
+        final Project project = DataKeys.PROJECT.getData(e.getDataContext());
+        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+            public void run() {
+                int count = 0;
+                for (final Module module : IntellijUtils.getAllModulesWithIvyIdeaFacet(project)) {
+                    final ModifiableRootModel model = ModuleRootManager.getInstance(module).getModifiableModel();
+                    final LibraryTable moduleLibraryTable = model.getModuleLibraryTable();
+                    final Library library = moduleLibraryTable.getLibraryByName(IvyIdeaTempConfiguration.getCurrent().getCreatedLibraryName());
+                    if (library != null) {
+                        moduleLibraryTable.removeLibrary(library);
+                        count++;
+                        model.commit();
+                    }
+                }
+                // TODO: Show dialog: "{0} libraries removed."                 
+            }
+        });
+    }
+
+}
---
+++ tags/0.4-alpha/src/org/clarent/ivyidea/resolve/ExternalDependency.java
@@ -0,0 +1,71 @@
+package org.clarent.ivyidea.resolve;
+
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+/**
+ * @author Guy Mahieu
+ */
+
+public abstract class ExternalDependency implements ResolvedDependency {
+
+    private static final Logger LOGGER = Logger.getLogger(ExternalJarDependency.class.getName());
+
+    protected File externalArtifact;
+
+    public ExternalDependency(File externalArtifact) {
+        this.externalArtifact = externalArtifact;
+    }
+
+    public File getExternalArtifact() {
+        return externalArtifact;
+    }
+
+    public void addTo(ModifiableRootModel moduleModel, Library.ModifiableModel libraryModel) {
+        final String artifactPath = externalArtifact.getAbsolutePath();
+        if (isMissing()) {
+            LOGGER.warning("Not registering external " + getTypeName() + " file dependency as the file does not seem to exist: " + artifactPath);
+            return;
+        }
+        if (isAlreadyRegistered(libraryModel)) {
+            LOGGER.info("Not re-registering external " + getTypeName() + " file dependency " + artifactPath + " as it is already part of the " + libraryModel.getName() + " library");
+            return;
+        }
+        LOGGER.info("Registering external " + getTypeName() + " file dependency: " + artifactPath);
+        libraryModel.addRoot(getUrlForLibrary(), getType());
+    }
+
+    @NotNull
+    protected String getUrlForLibrary() {
+        return "jar://" + externalArtifact.getAbsolutePath() + "!/";
+    }
+
+    protected boolean isMissing() {
+        return !new File(externalArtifact.getAbsolutePath()).exists();
+    }
+
+    protected boolean isAlreadyRegistered(Library.ModifiableModel libraryModel) {
+        // TODO: check if this naive check is good enough - is there a better way to do this?
+        final String artifactPath = externalArtifact.getAbsolutePath();
+        final VirtualFile[] files = libraryModel.getFiles(getType());
+        for (VirtualFile file : files) {
+            final String existingDependencyPath = file.getFileSystem().extractPresentableUrl(file.getPath());
+            // Compare the files not just the paths
+            // TODO: Are these paths always absolute??
+            if (new File(existingDependencyPath).equals(new File(artifactPath))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected abstract String getTypeName();
+
+    protected abstract OrderRootType getType();
+}

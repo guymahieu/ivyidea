5459e7d6d4d8dfe98f5667a2e660a7bcf669dc32
guy.mahieu <guy.mahieu@66ac992a-87ea-11dd-a302-d386e5130c1c>
2008-10-24 18:05:33 +0000

    Implemented rendering of deprecated configuration using the STRIKETHROUGH attribute. (as is the standard way of rendering deprecated things in IDEA)

--- trunk/src/org/clarent/ivyidea/intellij/facet/ui/components/ConfigurationSelectionTable.java
+++ trunk/src/org/clarent/ivyidea/intellij/facet/ui/components/ConfigurationSelectionTable.java
@@ -5,7 +5,12 @@ import com.intellij.ui.BooleanTableCellRenderer;
 import com.intellij.util.ui.Table;
 import org.apache.ivy.core.module.descriptor.Configuration;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableModel;
+import java.awt.*;
+import java.awt.font.TextAttribute;
+import java.util.HashMap;
 import java.util.Set;
 
 /**
@@ -31,6 +36,33 @@ public class ConfigurationSelectionTable extends Table {
     private void initComponents() {
         getColumnModel().getColumn(0).setCellRenderer(new BooleanTableCellRenderer());
         getColumnModel().getColumn(0).setCellEditor(new BooleanTableCellEditor());
+
+        // Register custom renderer to draw deprecated configs in 'strikethrough'
+        getColumnModel().getColumn(1).setCellRenderer(new DefaultTableCellRenderer() {
+
+            private Font regularFont;
+            private Font strikethroughFont;
+
+            // implements javax.swing.table.TableCellRenderer
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                final ConfigurationSelectionTableModel tableModel = (ConfigurationSelectionTableModel) table.getModel();
+                final Component rendererComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                if (regularFont == null) {
+                    regularFont = rendererComponent.getFont();
+                }
+                if (tableModel.getConfigurationAt(row).getDeprecated() != null) {
+                    if (strikethroughFont == null) {
+                        final HashMap<TextAttribute, Object> attribs = new HashMap<TextAttribute, Object>();
+                        attribs.put(TextAttribute.STRIKETHROUGH, Boolean.TRUE);
+                        strikethroughFont = regularFont.deriveFont(attribs);
+                    }
+                    rendererComponent.setFont(strikethroughFont);
+                } else {
+                    rendererComponent.setFont(regularFont);
+                }
+                return rendererComponent;
+            }
+        });
     }
 
 }

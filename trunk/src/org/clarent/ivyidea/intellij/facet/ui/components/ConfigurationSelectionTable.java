c391aae94aa5579ba7fa76471afacfe9cdeda431
guy.mahieu <guy.mahieu@66ac992a-87ea-11dd-a302-d386e5130c1c>
2008-10-24 17:05:39 +0000

    Added checkbox renderer/editor to the table to allow selecting configs to be resolved.

--- trunk/src/org/clarent/ivyidea/intellij/facet/ui/components/ConfigurationSelectionTable.java
+++ trunk/src/org/clarent/ivyidea/intellij/facet/ui/components/ConfigurationSelectionTable.java
@@ -1,6 +1,10 @@
 package org.clarent.ivyidea.intellij.facet.ui.components;
 
+import com.intellij.ui.BooleanTableCellEditor;
+import com.intellij.ui.BooleanTableCellRenderer;
 import com.intellij.util.ui.Table;
+
+import javax.swing.table.TableModel;
 
 /**
  * @author Guy Mahieu
@@ -10,6 +14,18 @@ public class ConfigurationSelectionTable extends Table {
 
     public ConfigurationSelectionTable() {
         super(new ConfigurationSelectionTableModel());
+        initComponents();
     }
+
+    public void setModel(TableModel dataModel) {
+        super.setModel(dataModel);
+        initComponents();
+    }
+
+    private void initComponents() {
+        getColumnModel().getColumn(0).setCellRenderer(new BooleanTableCellRenderer());
+        getColumnModel().getColumn(0).setCellEditor(new BooleanTableCellEditor());
+    }
+
 
 }

654b6b5baf74461aa5a1c388b2b168a4539ccca7
guy.mahieu <guy.mahieu@66ac992a-87ea-11dd-a302-d386e5130c1c>
2008-10-27 19:08:09 +0000

    Attempt to do 'resolve active' outside of event dispatch thread. TODO: check for better way + also check other lengthy operations + refactor code for clarity where what is executed

---
+++ trunk/src/org/clarent/ivyidea/resolve/Resolver.java
@@ -0,0 +1,29 @@
+package org.clarent.ivyidea.resolve;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.module.Module;
+import org.clarent.ivyidea.ivy.IvyManager;
+
+import java.util.List;
+
+/**
+ * @author Guy Mahieu
+ */
+public class Resolver {
+
+    private List<ResolvedDependency> dependencies;
+    private IvyManager ivyManager;
+
+    public Resolver(IvyManager ivyManager) {
+        this.ivyManager = ivyManager;
+    }
+
+    public List<ResolvedDependency> resolve(final Module module) {
+        ApplicationManager.getApplication().runReadAction(new Runnable() {
+            public void run() {
+                dependencies = new DependencyResolver().resolve(module, ivyManager);
+            }
+        });
+        return dependencies;
+    }
+}

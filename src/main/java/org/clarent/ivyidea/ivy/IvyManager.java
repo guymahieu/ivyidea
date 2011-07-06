d6d394b1df6d68fd2a40df8bd124a826edb7fe7d
maarten.coene <maarten.coene@gmail.com>
2011-07-06 11:38:12 +0000

    Bugfix: Ivy triggers were not called when resolving dependencies from IvyIDEA.

--- src/main/java/org/clarent/ivyidea/ivy/IvyManager.java
+++ src/main/java/org/clarent/ivyidea/ivy/IvyManager.java
@@ -20,13 +20,20 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.module.Module;
 import org.apache.ivy.Ivy;
+import org.apache.ivy.core.event.EventManager;
 import org.apache.ivy.core.settings.IvySettings;
+import org.apache.ivy.plugins.resolver.BasicResolver;
+import org.apache.ivy.plugins.trigger.Trigger;
 import org.clarent.ivyidea.config.IvyIdeaConfigHelper;
 import org.clarent.ivyidea.exception.IvySettingsFileReadException;
 import org.clarent.ivyidea.exception.IvySettingsNotFoundException;
 import org.clarent.ivyidea.intellij.IntellijUtils;
 import org.clarent.ivyidea.logging.ConsoleViewMessageLogger;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,6 +49,10 @@ public class IvyManager {
         if (!configuredIvyInstances.containsKey(module)) {
             final IvySettings configuredIvySettings = IvyIdeaConfigHelper.createConfiguredIvySettings(module);
             final Ivy ivy = Ivy.newInstance(configuredIvySettings);
+            
+            // we should now call the Ivy#postConfigure() method, but it is private :-(
+            // so we have to execute the same code ourselfs
+            postConfigure(ivy);
 
             registerConsoleLogger(module, ivy);
 
@@ -50,6 +61,22 @@ public class IvyManager {
         return configuredIvyInstances.get(module);
     }
 
+    private void postConfigure(final Ivy ivy) {
+        EventManager eventManager = ivy.getEventManager();
+        Collection triggers = ivy.getSettings().getTriggers();
+        for (Iterator iter = triggers.iterator(); iter.hasNext();) {
+            Trigger trigger = (Trigger) iter.next();
+            eventManager.addIvyListener(trigger, trigger.getEventFilter());
+        }
+        
+        for (Iterator iter = ivy.getSettings().getResolvers().iterator(); iter.hasNext();) {
+            Object resolver = iter.next();
+            if (resolver instanceof BasicResolver) {
+                ((BasicResolver) resolver).setEventManager(eventManager);
+            }
+        }
+    }
+    
     private void registerConsoleLogger(final Module module, final Ivy ivy) {
         ApplicationManager.getApplication().invokeLater(new Runnable() {
             public void run() {
